9b8d7bc44965b0a1da44661b54cf3e847139987e
Niranjit123 <sanasamniranjit1@gmail.com>
2026-08-11 12:49:05 +0530

    ruff fix again

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
-from typing import AsyncGenerator
 
 from fastapi import FastAPI
 
--- app/messaging/connection.py
+++ app/messaging/connection.py
@@ -1,5 +1,5 @@
 import aio_pika
-from aio_pika.abc import AbstractRobustConnection, AbstractChannel
+from aio_pika.abc import AbstractChannel, AbstractRobustConnection
 
 from app.config import settings
 
